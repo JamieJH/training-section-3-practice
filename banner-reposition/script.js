a3edbd1cf20c41ca2b14989ed1eb1b2ab78f4772
jamieminh <48439646+jamieminh@users.noreply.github.com>
2021-07-06 13:26:05 +0700

    First commit

---
+++ banner-reposition/script.js
@@ -0,0 +1,97 @@
+(function () {
+    const image = document.querySelector(".banner__image")
+
+    // set initial img, zoom level and position taken from local storage (if available)
+    const uploadedImage = document.querySelector('#uploaded-img');
+    const defaultImageSrc = "https://images.wallpaperscraft.com/image/city_art_sky_127834_1600x900.jpg"
+    const initialPosition = localStorage.getItem("position") || "0,0"
+    let zoomLevel = localStorage.getItem("zoom") || 1
+
+    uploadedImage.src = localStorage.getItem("imageSrc") || defaultImageSrc
+    uploadedImage.style.left = initialPosition.split(",")[0] + "px"
+    uploadedImage.style.top = initialPosition.split(",")[1] + "px"
+    uploadedImage.style.transform = "scale(" + zoomLevel + ")"
+
+    // --------------------------------------------------
+    // Upload File
+    const fileInput = document.querySelector('input[type="file"]')
+
+    fileInput.addEventListener("change", (e) => {
+        console.log(e);
+        console.log(e.target.files);
+        if (e.target.files && e.target.files[0]) {
+            e.target.files[0].onload = () => {
+                URL.revokeObjectURL(uploadedImage.src);  // no longer needed, free memory
+            }
+            uploadedImage.src = URL.createObjectURL(e.target.files[0]); // set src to blob url
+        }
+    })
+
+
+    // --------------------------------------------------
+    //  Reposition using mouse movement
+    const repositionBtn = document.querySelector("#reposition")
+    const saveBtn = document.querySelector("#save")
+    let isEditing = false
+    let isMouseDown = false
+    let offset = [0, 0]     // x, y
+    let mousePos
+
+
+    repositionBtn.addEventListener("click", () => {
+        isEditing = !isEditing
+        repositionBtn.innerHTML = isEditing ? "Cancel" : "Reposition"
+    })
+
+    image.addEventListener("mousedown", (e) => {
+        isMouseDown = true
+        // e.clientX : mouse down z position relative to the html page
+        // mage.offsetLeft: position of the image relative to the html page
+        offset = [image.offsetLeft - e.clientX, image.offsetTop - e.clientY]
+    })
+
+    image.addEventListener("mouseup", () => {
+        isMouseDown = false
+    })
+
+    image.addEventListener("mouseleave", () => {
+        isMouseDown = false
+    })
+
+    image.addEventListener("mousemove", (e) => {
+        if (isMouseDown && isEditing) {
+            mousePos = [e.clientX, e.clientY]
+            image.style.left = mousePos[0] + offset[0] + 'px'
+            image.style.top = mousePos[1] + offset[1] + 'px'
+        }
+    })
+
+    // save current position and zoom status in localstorage when click save
+    saveBtn.addEventListener("click", () => {
+        localStorage.setItem("position", [image.offsetLeft, image.offsetTop])
+        localStorage.setItem("zoom", zoomLevel)
+        localStorage.setItem("imageSrc", uploadedImage.src)
+    })
+
+
+    // --------------------------------------------------
+    //  Zoom in and out (scale)
+    image.addEventListener("wheel", (e) => {
+        if (isEditing) {
+            // get current scale value 
+            // boundindRect returns the actual dimenstion (scaled), offsetWidth returns unscaled dimension
+            let scale = image.getBoundingClientRect().width / image.offsetWidth;
+            scale = Math.round(scale * 10) / 10
+
+            // on mouse wheel up (zoom in), deltaY < 0, and vice versa
+            // add 0.1 if delta < 0 (zoom in), else substract 0.1
+            zoomLevel = scale + 0.1 * (e.deltaY < 0 || -1)
+
+            // scale range 1 to 4
+            if (zoomLevel >= 1 && zoomLevel <= 4) {
+                image.style.transform = "scale(" + zoomLevel + ")"
+            }
+        }
+
+    })
+})()
---
+++ story-carousel/script.js
@@ -0,0 +1,319 @@
+// (function () {
+
+// --------------------------------------------------
+// Upload Files
+const form = document.querySelector(".upload-form")
+const storySelect = form.querySelector("#story-select")
+const storyName = form.querySelector("#story-name")
+const fileInput = document.querySelector('input[type="file"]')
+const addBtn = document.querySelector("button#add")
+const storyFiles = {}
+
+// Story and Slide 
+const storyCarousel = document.querySelector(".story-carousel")
+const track = storyCarousel.querySelector(".carousel-track")
+const previsouBtn = storyCarousel.querySelector(".carousel-button.previous")
+const nextBtn = storyCarousel.querySelector(".carousel-button.next")
+const progressBars = storyCarousel.querySelector(".progress-bar-container")
+
+// Auto Slide variables
+const SLIDE_INTERVAL = 6000         // time between each slide (for auto sliding)
+let slideIntervalId;                // id of setInterval that before moving to the next slide
+let slideTimeoutId;                 // id of setTimeout to slide the remaining time after stop
+let fillProgressBarIntervalId;      // id of setInterval to fill progress bar during SLIDE_INTERVAL
+let currentBarIndex = 0;            // index of the progres bar to be filled
+let currentInnerWidth = 0;          // filled width of the current progress bar
+let isAutoPlaying = true;           // true is auto play is on
+let timePassed = 0;                 // time passed since the start of slide when user pause auto play
+
+// set up story select input
+function setupStorySelect() {
+    storySelect.innerHTML = `
+            <option value="">--- Choose story ---</option>
+            <option value="new">CREATE NEW STORY</option>`
+
+    Object.keys(storyFiles).forEach(title => {
+        storySelect.innerHTML += `<option value="${title}">${title}</option>`
+    })
+}
+
+// position slides next to each other
+function positionSlides() {
+    let totalSlides = 0
+    const userStories = storyCarousel.querySelectorAll(".user-story")
+    userStories.forEach(story => {
+        const slides = story.querySelectorAll(".user-story__slide")
+        slides.forEach((slide, slideIndex) => {
+            slide.style.left = (slideIndex + totalSlides) * 100 + "%"
+        })
+        totalSlides += slides.length
+    })
+}
+
+// create a story slide html element 
+function createStorySlide(storyTitle, slideInfo) {
+    let storyContainer = track.querySelector("#" + storyTitle);
+
+    // if this function is called when the story only has 1 slide, create the story container
+    if (storyFiles[storyTitle].length === 1) {
+        storyContainer = document.createElement("DIV");
+        storyContainer.className = "user-story " + (track.children.length === 0 ? "active" : "")
+        storyContainer.id = storyTitle
+    }
+
+    // create the slide container that contain the image/video
+    const slideContainer = document.createElement("DIV")
+    // if this is the first ever story and frist ever slide, set the slide to active
+    slideContainer.className = "user-story__slide " +
+        ((track.children.length === 0 && storyFiles[storyTitle].length === 1) ? "active" : "");
+
+    let media = ""
+    if (slideInfo.type === "image") {
+        media = document.createElement("IMG")
+        media.className = "user-story__slide-media"
+        media.setAttribute("alt", slideInfo.name)
+        media.setAttribute("src", slideInfo.src)
+    }
+    else {
+        media = document.createElement("VIDEO")
+        media.className = "user-story__slide-media"
+        media.setAttribute("width", 400)
+        media.setAttribute("height", 700)
+
+        media.innerHTML = `
+            <source src="${slideInfo.src}" type="video/mp4">
+            <source src="${slideInfo.src}" type="video/mkv">
+            Your browser does not support the video tag.`
+    }
+
+    slideContainer.appendChild(media)
+    storyContainer.appendChild(slideContainer)
+    if (storyFiles[storyTitle].length === 1) {
+        track.appendChild(storyContainer)
+    }
+}
+
+
+// function to remove/add active class for slide container and slide
+function toggleActiveClass(oldACtiveItem, newActiveItem) {
+    oldACtiveItem.classList.remove("active")
+    newActiveItem.classList.add("active")
+}
+
+
+// function to play/pause a video, isPlayNow = true if the video should be played, otherwise false
+function toggleVideo(video, isPlayNow) {
+    if (video && video.tagName.toLowerCase() === "video") {
+        isPlayNow ? video.play() : video.pause()
+    }
+}
+
+// set auto slide after 6s, during this time, update the progress bar
+function startAutoSlide(startWidth = 0) {
+    isAutoPlaying = true
+    const timeLeft = SLIDE_INTERVAL - timePassed
+    fillRunningBar(Array.from(progressBars.children)[currentBarIndex], startWidth)
+
+    if (timeLeft !== SLIDE_INTERVAL) {
+        slideTimeoutId = setTimeout(() => slideUsingButtons(true), timeLeft)
+    }
+    slideIntervalId = setInterval(() => slideUsingButtons(true), 6000)
+
+    // if active slide contain a video, play it
+    toggleVideo(storyCarousel.querySelector(".user-story__slide.active > video"), true)
+}
+
+
+function stopAutoSlide() {
+    isAutoPlaying = false
+    clearInterval(fillProgressBarIntervalId)
+    clearTimeout(slideTimeoutId)
+    clearInterval(slideIntervalId)
+    // if active slide contain a video, pause it
+    toggleVideo(storyCarousel.querySelector(".user-story__slide.active > video"), false)
+}
+
+function setUpProgressBars(barCount) {
+    // draw progress bar
+    progressBars.innerHTML = ""
+    for (let i = 0; i < barCount; i++) {
+        progressBars.innerHTML += `
+            <div class="progress-bar" 
+                style="width: calc(100% / ${barCount} - 3px); ">
+                <div class="bar outer-bar"></div>
+                <div class="bar inner-bar"></div>
+            </div>
+            `
+    }
+}
+
+function fillRunningBar(progressBar, startWidth = 0) {
+    // clear old interval
+    currentInnerWidth = startWidth
+
+    fillProgressBarIntervalId = setInterval(() => {
+        // the amount of px being added to running bar width every 100s
+        const changeAmountPer100s = progressBar.offsetWidth * 100 / 6000 + 0.03
+        if (currentInnerWidth <= progressBar.offsetWidth) {
+            currentInnerWidth += changeAmountPer100s
+            progressBar.querySelector(".inner-bar").style.width = currentInnerWidth + "px"
+        }
+        else {
+            currentInnerWidth = 0
+            clearInterval(fillProgressBarIntervalId)
+        }
+        timePassed += 100
+    }, 100)
+}
+
+
+function moveToSlide(storyContainer, targetSlide, isNext, isSetupBarNecessary = false) {
+    const currentStory = storyCarousel.querySelector(".user-story.active")
+    // if target slide is not specified, slide to first slide of story
+    if (targetSlide === null) {
+        targetSlide = storyContainer.querySelector(".user-story__slide")
+        currentBarIndex = 0
+    }
+
+    // pause current slide video if it is a video
+    const currentVideo = currentStory.querySelector(".user-story__slide.active video")
+    if (currentVideo) {
+        currentVideo.pause()
+    }
+
+    // reload and play video if target slide is a video 
+    const targetMedia = targetSlide.querySelector("video")
+    if (targetMedia) {
+        targetMedia.load()
+        targetMedia.play()
+    }
+
+    if (isSetupBarNecessary) {
+        setUpProgressBars(storyContainer.children.length)
+    }
+
+    const allBars = Array.from(progressBars.children)
+
+    // fully fill previous progress bars 
+    for (let i = 0; i < currentBarIndex; i++) {
+        allBars[i].querySelector(".inner-bar").style.width = "100%"
+    }
+
+    // if user go back, unfill the following progress bars
+    if (!isNext) {
+        for (let i = currentBarIndex; i < allBars.length; i++) {
+            allBars[i].querySelector(".inner-bar").style.width = "0"
+        }
+    }
+
+    track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+    toggleActiveClass(currentStory, storyContainer)
+    toggleActiveClass(storyCarousel.querySelector(".user-story__slide.active"), targetSlide)
+    timePassed = 0
+    startAutoSlide()
+}
+
+
+function slideUsingButtons(isNext) {
+    let activeStory = storyCarousel.querySelector(".user-story.active")
+    let activeSlide = activeStory.querySelector(".active")
+
+    // get previous or next slide as target slide
+    let targetSlide = isNext ? activeSlide.nextElementSibling : activeSlide.previousElementSibling
+    let siblingStory = null
+
+    stopAutoSlide()       // turn off auto slide in case user slide when progress bar was being filled
+
+    // if there's no sibling slide, check for sibling story
+    if (!targetSlide) {
+        siblingStory = isNext ? activeStory.nextElementSibling : activeStory.previousElementSibling
+        // if there's sibling story, set target slide to first slide of that story (next), or last if previous
+        if (siblingStory) {
+            targetSlide = isNext ? siblingStory.querySelector(".user-story__slide:first-child") :
+                siblingStory.querySelector(".user-story__slide:last-child");
+
+            // get index of progress bar to later fill it
+            currentBarIndex = isNext ? 0 : siblingStory.children.length - 1
+
+            // move to the target slide, and set new active for target slide and sibling story
+            moveToSlide(siblingStory, targetSlide, isNext, true)
+        }
+    }
+    else {
+        currentBarIndex += isNext || -1                 // get index of progress bar to later fill it
+        moveToSlide(activeStory, targetSlide, isNext)   // move to target slide, and set it to active   
+    }
+}
+
+// disable the story name input text field if user select an available story
+storySelect.addEventListener("change", (e) => {
+    const option = e.target.value;
+    (option === "new") ? storyName.removeAttribute("disabled") : storyName.setAttribute("disabled", "true")
+})
+
+
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    // deal with input error
+    if (storySelect.value === "") {
+        alert("Please choose a story.")
+        return
+    }
+    if (storySelect.value === "new" && storyName.value.trim() === "") {
+        alert("Please add a title for your new story.")
+        return
+    }
+    if (storySelect.value === "new" && storyFiles[storyName.value.trim().toLowerCase()]) {
+        alert("Please enter a new unique story name.")
+        return
+    }
+    if (fileInput.files.length === 0) {
+        alert("Please upload some file(s).")
+        return
+    }
+
+    // All inputs are correct
+    let isAllGood = true;   // to check if all input is <= 10MB
+    const enteredStoryTitle = storySelect.value === "new" ? storyName.value.trim().toLowerCase() : storySelect.value
+    if (storySelect.value === "new") {
+        storyFiles[enteredStoryTitle] = []        // initialise an array
+    }
+
+    Array.from(fileInput.files).every(file => {
+        // if file size > 5MB
+        if (file.size > 10 * 1000000) {
+            alert("Some file size is larger than 10MB");
+            isAllGood = false
+            return false
+        }
+        const newSlide = {
+            name: file.name,
+            src: URL.createObjectURL(file),
+            type: file.type.split("/")[0]
+        }
+        storyFiles[enteredStoryTitle].push(newSlide)
+        createStorySlide(enteredStoryTitle, newSlide)
+        return true
+    })
+
+    if (isAllGood) {
+        positionSlides()            // positions slides    
+        moveToSlide(storyCarousel.querySelector("#" + enteredStoryTitle), null, true, true)       // move to the story first slide
+        setupStorySelect()          // update the select box
+
+        storyCarousel.style.display = "block"       // display story carousel
+
+        // clear inputs 
+        storyName.value = ""
+        fileInput.value = ""
+    }
+})
+
+track.addEventListener("mousedown", stopAutoSlide)
+track.addEventListener("mouseup", () => startAutoSlide(currentInnerWidth))
+previsouBtn.addEventListener("click", () => slideUsingButtons(false))
+nextBtn.addEventListener("click", () => slideUsingButtons(true))
+
+setupStorySelect()
+
+// })()
