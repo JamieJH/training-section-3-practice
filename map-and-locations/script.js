b4e723e3a174309bc0281e4aaa5fbd97003915e9
JamieJH <hue.nguyen@journeyh.com>
2021-07-18 22:34:01 +0700

    Refactor: Rename varibles and remove redundant comments - Use the word "route" instead of "directions" in naming functions related to finding routes between two markers. - Rename variables and functions to be self-descriptive. - Line 162 - 170: separate text from backticks to make it clearer for future changes in terms of author. - Keep informative comments and remove the rest. - Not add semicolons as they would cause too many uneccessary lines changes.

--- map-and-locations/script.js
+++ map-and-locations/script.js
@@ -3,23 +3,23 @@
     const URL = 'https://api.mapbox.com'
     const locationInput = document.getElementById("location")
     const resultList = document.getElementById("search-results")
-    const helpBtn = document.querySelector(".btn-help")
-    const clearBtn = document.querySelector(".btn-clear")
-    const directionsBtn = document.querySelector(".btn-directions")
-    const clearRouteBtn = document.querySelector(".btn-clear-route")
-    const clearInputBtn = document.querySelector(".btn-clear-input")
+    const helpButton = document.querySelector(".btn-help")
+    const clearButton = document.querySelector(".btn-clear")
+    const directionsToggleButton = document.querySelector(".btn-directions")
+    const clearRouteButton = document.querySelector(".btn-clear-route")
+    const clearInputButton = document.querySelector(".btn-clear-input")
     const instructionsModal = document.querySelector(".instructions-modal")
     const statusBox = document.querySelector(".status-box")
     const mapContainer = document.getElementById("map")
     let map;
     let isDirectionEnabled = false;
-    let searchResults = {}              // object to store at most 5 search results
-    const markersInfo = []              // array to store markers as regular objects similar to a search result (except the original center)
-    const directionsEndPoints = []      // store 2 points (markerInfo obj) to show directions between them
-    let markerPoints = {}               // array to store markers as map marker objs (except the original center)
-    let isInstructionOpen = false       // true if the instructions modal is visible (open)
-    let markerClicked = false           // true if a marker is clicked
-    let debounceId                      // setTimout id to debounce input location search
+    let searchResults = {}              // at most 5 search results
+    const markersInfo = []              // does not store original marker
+    const routeTwoEndPoints = []
+    let mapMarkerInstances = {}           // does not store original marker
+    let isInstructionOpen = false
+    let markerClicked = false
+    let inputDebounceTimeoutId
 
     // coordinates to create a bounding box to limit search zone, the box corners are:
     // top left: (minX, maxY); top right: (max, max); bottom left: (min, min), bottom right: (maxX, miY)
@@ -30,7 +30,6 @@
         maxY: 11.159164
     }
 
-    // default marker if user refuse to allow location
     let originalMarker = {
         id: "original",
         name: "Journey Horizon",
@@ -68,24 +67,23 @@
         setupMap()
     }
 
-    function populateResultList(isError = false) {
+    function showResultList(isError = false) {
         resultList.innerHTML = ""
         if (isError || Object.keys(searchResults).length === 0) {
-            const li = document.createElement("li")
-            li.innerText = isError ? "Something went wrong. Please try again later!" : "No result found!"
-            resultList.append(li)
+            const listItem = document.createElement("li")
+            listItem.innerText = isError ? "Something went wrong. Please try again later!" : "No result found!"
+            resultList.append(listItem)
             return
         }
         for (const placeId in searchResults) {
-            const li = document.createElement("li")
-            li.setAttribute("id", placeId)
-            li.innerText = searchResults[placeId].name
-            resultList.append(li)
+            const listItem = document.createElement("li")
+            listItem.setAttribute("id", placeId)
+            listItem.innerText = searchResults[placeId].name
+            resultList.append(listItem)
         }
     }
 
-    // function to transform a 'feature' received from api call to an obj with required values
-    function featureToLocation(feature) {
+    function featureToLocationConverter(feature) {
         const subLocations = feature.context
         const address = { full: feature.properties.address }
 
@@ -109,17 +107,17 @@
                 if (res.status === 200 || res.ok) {
                     return res.json()
                 }
-                populateResultList(isError = true)
+                showResultList(isError = true)
             })
             .then(data => {
                 searchResults = {}
                 data.features.forEach(place => {
                     const id = place.id.split(".")[1]
-                    searchResults[id] = featureToLocation(place)
+                    searchResults[id] = featureToLocationConverter(place)
                 })
-                populateResultList()
+                showResultList()
             })
-            .catch(() => populateResultList(isError = true))
+            .catch(() => showResultList(isError = true))
     }
 
     function removeRoute() {
@@ -131,7 +129,6 @@
     }
 
     function drawRoute(coordinates) {
-        // if there's already a route, remove it
         removeRoute()
         map.addSource('route', {
             'type': 'geojson',
@@ -162,26 +159,20 @@
     function displayStatusBox(isError, duration, distance) {
         statusBox.classList.add("show")
         statusBox.innerHTML = (!isError) ?
-            `<h3>Directions (Driving)</h3>
-            <p>Distance: ${distance.km}km ${distance.meters}m</p>
-            <p>Duration: ${duration.hours} hour(s) ${duration.minutes} minute(s)</p>` :
-            `<h3>Error</h3>
-            <p>Something went wrong. Please try again later!</p>`
+            `
+                <h3>Directions (Driving)</h3>
+                <p>Distance: ${distance.km}km ${distance.meters}m</p>
+                <p>Duration: ${duration.hours} hour(s) ${duration.minutes} minute(s)</p>
+            ` :
+            `
+                <h3>Error</h3>
+                <p>Something went wrong. Please try again later!</p>
+            `
     }
 
-    function displayStatusBox(isError, duration, distance) {
-        statusBox.classList.add("show")
-        statusBox.innerHTML = (!isError) ?
-            `<h3>Directions (Driving)</h3>
-            <p>Distance: ${distance.km}km ${distance.meters}m</p>
-            <p>Duration: ${duration.hours} hour(s) ${duration.minutes} minute(s)</p>` :
-            `<h3>Error</h3>
-            <p>Something went wrong. Please try again later!</p>`
-    }
-
-    function findDirections() {
-        const coordinatesString = directionsEndPoints.map(point => point.coordinates.join(",")).join(";")
-        fetch(`${URL}/directions/v5/mapbox/driving/${coordinatesString}?geometries=geojson&access_token=${ACCESS_TOKEN}`)
+    function findRoute() {
+        const coordinatesAsString = routeTwoEndPoints.map(point => point.coordinates.join(",")).join(";")
+        fetch(`${URL}/directions/v5/mapbox/driving/${coordinatesAsString}?geometries=geojson&access_token=${ACCESS_TOKEN}`)
             .then(res => {
                 if (res.status === 200 || res.ok) {
                     return res.json()
@@ -190,13 +181,13 @@
             })
             .then(data => {
                 const coordinates = data.routes[0].geometry.coordinates
-                const duration = data.routes[0].duration    // time in seconds
-                const distance = Math.floor(data.routes[0].distance)    // distance in meters
+                const durationInSeconds = data.routes[0].duration
+                const distanceInMeters = Math.floor(data.routes[0].distance)
 
-                const hours = Math.floor(duration / 3600)
-                const minutes = Math.floor((duration % 3600) / 60)
-                const km = Math.floor(distance / 1000)
-                const meters = distance % 1000
+                const hours = Math.floor(durationInSeconds / 3600)
+                const minutes = Math.floor((durationInSeconds % 3600) / 60)
+                const km = Math.floor(distanceInMeters / 1000)
+                const meters = distanceInMeters % 1000
 
                 drawRoute(coordinates)
                 displayStatusBox(false, { hours, minutes }, { km, meters })
@@ -204,43 +195,41 @@
             .catch(() => displayStatusBox(true))
     }
 
-    function addPointAndFindDirections(markerInfo) {
-        if (directionsEndPoints.length === 2) {
-            const firstMarker = directionsEndPoints.shift()   // remove the first item
-            markerPoints[firstMarker.id].getElement().classList.remove("selected")
+    function addRouteEndPoint(markerInfo) {
+        if (routeTwoEndPoints.length === 2) {
+            const firstMarker = routeTwoEndPoints.shift()   // remove the first item
+            mapMarkerInstances[firstMarker.id].getElement().classList.remove("selected")
         }
 
-        directionsEndPoints.push(markerInfo)
+        routeTwoEndPoints.push(markerInfo)
         const markerElement = document.querySelector(".marker#" + markerInfo.id)
         markerElement.classList.add("selected")
-        if (directionsEndPoints.length === 2) {
-            findDirections()
+        if (routeTwoEndPoints.length === 2) {
+            findRoute()
         }
     }
 
     function removeMarkerById(markerId) {
-        markerPoints[markerId].remove()
-        delete markerPoints[markerId]
+        mapMarkerInstances[markerId].remove()
+        delete mapMarkerInstances[markerId]
 
-        // remove route if the marker is part of a route
-        const index = directionsEndPoints.findIndex(point => markerId === point.id)
+        const index = routeTwoEndPoints.findIndex(point => markerId === point.id)
         if (index !== -1) {
             removeRoute()
-            directionsEndPoints.splice(index, 1)
+            routeTwoEndPoints.splice(index, 1)
         }
     }
 
     function removeAllMarkers() {
-        Object.values(markerPoints).forEach(marker => marker.remove())
-        markerPoints = {}
+        Object.values(mapMarkerInstances).forEach(marker => marker.remove())
+        mapMarkerInstances = {}
         markersInfo.length = 0
         removeRoute()
-        directionsEndPoints.length = 0
+        routeTwoEndPoints.length = 0
     }
 
     function addMarker(markerInfo) {
         markerClicked = false
-        // if directions is disabled, user can only have one active marker at a time
         if (!isDirectionEnabled) {
             removeAllMarkers()
         }
@@ -261,7 +250,6 @@
             }
         }
 
-        // center on the selected marker
         map.flyTo({
             center: markerInfo.coordinates
         })
@@ -285,45 +273,41 @@
             .addTo(map);
 
         if (isDirectionEnabled) {
-            addPointAndFindDirections(markerInfo)
+            addRouteEndPoint(markerInfo)
         }
 
-        // on clicing a marker, if directionsEndPoints has 2 points, show a route between then
         markerPoint.getElement().addEventListener("click", () => {
             if (isDirectionEnabled) {
-                // if marker is already in directionsEndPoints, remove it and remove the route
-                const index = directionsEndPoints.findIndex(point => {
+                const index = routeTwoEndPoints.findIndex(point => {
                     return markerInfo.coordinates[0] === point.coordinates[0] && markerInfo.coordinates[1] === point.coordinates[1]
                 })
 
                 if (index !== -1) {
-                    document.getElementById(directionsEndPoints[index].id).classList.remove("selected")
-                    directionsEndPoints.splice(index, 1)
+                    document.getElementById(routeTwoEndPoints[index].id).classList.remove("selected")
+                    routeTwoEndPoints.splice(index, 1)
                     removeRoute()
                 }
                 else {
-                    addPointAndFindDirections(markerInfo)
+                    addRouteEndPoint(markerInfo)
                 }
             }
             markerClicked = true
         })
 
         markersInfo.push(markerInfo)
-        markerPoints[markerInfo.id] = markerPoint
+        mapMarkerInstances[markerInfo.id] = markerPoint
     }
 
-    // search for location once user has entered at least 3 letters
     locationInput.addEventListener("input", (e) => {
-        clearTimeout(debounceId)
+        clearTimeout(inputDebounceTimeoutId)
         resultList.classList.add("show")
         if (e.target.value.length >= 3) {
-            debounceId = setTimeout(() => {
+            inputDebounceTimeoutId = setTimeout(() => {
                 locationSearch(e.target.value)
             }, 1000)
         }
     })
 
-    // add a marker upon clicking a search result
     resultList.addEventListener("click", (e) => {
         if (e.target.tagName === 'LI') {
             const placeId = e.target.getAttribute("id")
@@ -334,54 +318,47 @@
     })
 
 
-    // clear all markers, directions and center back to the original center
-    clearBtn.addEventListener("click", () => {
+    clearButton.addEventListener("click", () => {
         removeAllMarkers()
         addMarker(originalMarker)
     })
 
-    // clear input
-    clearInputBtn.addEventListener("click", () => {
+    clearInputButton.addEventListener("click", () => {
         locationInput.value = ""
     })
 
-    // toggle direction button to start looking for a route between 2 points
-    directionsBtn.addEventListener("click", () => {
+    directionsToggleButton.addEventListener("click", () => {
         isDirectionEnabled = !isDirectionEnabled
         if (isDirectionEnabled) {
-            directionsBtn.classList.add("on")
-            directionsBtn.innerHTML = "Disable Directions"
+            directionsToggleButton.classList.add("on")
+            directionsToggleButton.innerHTML = "Disable Directions"
         }
         else {
-            directionsBtn.classList.remove("on")
-            directionsBtn.innerHTML = "Enabled Directions"
+            directionsToggleButton.classList.remove("on")
+            directionsToggleButton.innerHTML = "Enabled Directions"
         }
     })
 
-    // clear all routes button, also clear the directionsEndPoints array
-    clearRouteBtn.addEventListener("click", () => {
-        directionsEndPoints.forEach(point => {
+    clearRouteButton.addEventListener("click", () => {
+        routeTwoEndPoints.forEach(point => {
             document.getElementById(point.id).classList.remove("selected")
         })
-        directionsEndPoints.length = 0
+        routeTwoEndPoints.length = 0
         removeRoute()
     })
 
-    // remove marker when clicing on the remove marker button on the popup
     mapContainer.addEventListener("click", (e) => {
         if (e.target.className === "btn-remove-marker") {
             removeMarkerById(e.target.getAttribute("for"))
         }
     })
 
-    // function to display or hide the instruction modal
     function toggleInstructions(isOn) {
         isInstructionOpen = isOn;
         isInstructionOpen ? instructionsModal.classList.add("show") : instructionsModal.classList.remove("show")
     }
 
-    // the instruction modal will appear upon clicking the help button, click it again to close modal
-    helpBtn.addEventListener("click", () => toggleInstructions(!isInstructionOpen))
+    helpButton.addEventListener("click", () => toggleInstructions(!isInstructionOpen))
     instructionsModal.querySelector(".btn-close-modal").addEventListener("click", () => toggleInstructions(false))
     instructionsModal.querySelector(".instructions-backdrop").addEventListener("click", () => toggleInstructions(false))
 
@@ -396,7 +373,7 @@
         // set the bounds of the map [[southwest coordinates], [northeast coordinates]]
         map.setMaxBounds([[BBOX.minX - 0.1, BBOX.minY - 0.1], [BBOX.maxX + 0.1, BBOX.maxY + 0.1]]);
         map.addControl(new mapboxgl.NavigationControl());   // Add zoom and rotation controls
-        addMarker(originalMarker)                           // add marker for original center
+        addMarker(originalMarker)
 
         map.on('click', (e) => {
             // when a user click is not on a marker, create a marker of that point
@@ -422,7 +399,7 @@
                         // only take the nearest feature's coordinates when it is not too far
                         // else take the on click coordinates (meaning there's no other info like address or name)
                         if (Math.abs(clickedCoors[0] - featureCoors[0]) <= 0.03 && Math.abs(clickedCoors[1] - featureCoors[1]) <= 0.03) {
-                            location = featureToLocation(data.features[0])
+                            location = featureToLocationConverter(data.features[0])
                         }
                         statusBox.classList.remove("show")
 
